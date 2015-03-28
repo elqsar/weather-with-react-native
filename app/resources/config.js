e0334258d2aaa6610abf8258ffffc4205ca981ec
Boris Musatov <boris.musatov@gmail.com>
2015-03-28 17:12:29 +0100

    Fix API key

--- app/resources/config.js
+++ app/resources/config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	BASE_URL: 'https://api.forecast.io/forecast/',
-	API_KEY: '69e58003b47432e6aff9568bae68d2f9/'
+	API_KEY: '69e58003b47432e6aff9568bae68d2f9'
 }
--- app/services/ForecastApi.js
+++ app/services/ForecastApi.js
@@ -7,7 +7,7 @@ class ForecastApi {
 	query(options, callback) {
 		var longitude = options.longitude || 50;
 		var latitude = options.latitude || 14;
-		var url = config.BASE_URL + config.API_KEY + [latitude, longitude].join(',') + '?units=si';
+		var url = config.BASE_URL + config.API_KEY + '/' + [latitude, longitude].join(',') + '?units=si';
 
 		console.log('Call forecast.io ', url);
 		fetch(url)
